a8df821bfcc7db36d6fc8229428c3d6d5f314356
David Diaz <david.ecommerce99@gmail.com>
2025-03-03 22:45:43 -0500

    Tutorial: graphql-client/mutation - addUser

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useQuery, gql } from "@apollo/client";
+import CreateUser from "../mutation/page";
 
 const GET_USERS = gql`
   query GetUsers {
@@ -18,15 +19,30 @@ export default function Users() {
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div>
-      <h1>Users List</h1>
-      <ul>
-        {data.users.map((user: { id: string; name: string; age: number }) => (
-          <li key={user.id}>
-            {user.name} - {user.age}
-          </li>
-        ))}
-      </ul>
+    <div className="p-4 space-y-4">
+      <CreateUser />
+      <hr />
+      <div>
+        <h1>Users List</h1>
+        <table className="w-full border border-gray-300">
+          <thead>
+            <tr className="bg-gray-200">
+              <th>ID</th>
+              <th>Name</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.users.map((user: { id: string; name: string; age: number }) => (
+              <tr key={user.id} className="border border-gray-300 even:bg-gray-100">
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.age}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
